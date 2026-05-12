90a34679dd2e039912ac7756eb8cf17da25b8907
averniere <aurane.verniere@ensae.fr>
2026-05-12 12:27:45 +0000

    code qui marche pour calculer l'information content

--- Embeddings/data.py
+++ Embeddings/data.py
@@ -8,7 +8,11 @@ data_path = os.path.join(current_dir, "..", "data", "HPOs.csv")
 
 df_hpo = pd.read_csv(data_path, sep=";")
 df_hpo = df_hpo.drop(columns = ["definition", "synonyms"])
-df_hpo['parents'] = df_hpo['parents'].apply(ast.literal_eval)
+# df_hpo['parents'] = df_hpo['parents'].apply(ast.literal_eval)
+df_hpo['hp_id'] = df_hpo['hp_id'].str.strip("'\"")
+df_hpo['parents'] = df_hpo['parents'].apply(
+    lambda lst: [p.strip("'\"") for p in ast.literal_eval(lst)] 
+    if isinstance(lst, str) else [p.strip("'\"") for p in lst])
 
 G_hpo = nx.DiGraph()
 
